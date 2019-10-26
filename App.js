217f3771b24fb2a3cd88c16834bb70f163c7a416
Edwin Ushibantu <51169097+Edd-Venv@users.noreply.github.com>
2019-10-26 17:51:57 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,48 +1,74 @@
 import React from "react";
 import "./App.css";
-import axios from "axios";
-import Header from "./components/Header.js";
-import Search from "./components/Search.js";
-import Body from "./components/Body.js";
+import Header from "./components/Header/Header.js";
+import Search from "./components/Search/Search.js";
+import Body from "./components/Body/Body.js";
+import MovieSearch from "./components/MovieSearch/MovieSearch.js";
+import Movies from "./components/Movies/Movies.js";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false
-    };
-  }
-
   render() {
     return (
       <React.Fragment>
-        <h2
+        <div
           style={{
-            marginLeft: "43%",
-            fontSize: "1.5em",
-            fontWeight: "bolder",
-            color: "white"
+            maxWidth: "1000px",
+            minWidth: "50%",
+            margin: "0 auto"
           }}
         >
-          TRENDING SHOWS
-        </h2>
-        <br />
-        <br />
-        <Header />
-        <br />
-        <br />
-        <Body />
-        <Search />
-        <h2
-          style={{
-            marginLeft: "40%",
-            fontSize: "1.5em",
-            fontWeight: "bolder",
-            color: "white"
-          }}
-        >
-          CURRENT TOP FOUR SHOWS
-        </h2>
+          <h2
+            style={{
+              textAlign: "center",
+              fontSize: "1.5em",
+              fontWeight: "bolder",
+              color: "white"
+            }}
+          >
+            TRENDING SHOWS
+          </h2>
+          <br />
+          <br />
+          <Header />
+          <br />
+          <Body />
+
+          <br />
+          <br />
+          <Search />
+
+          <h2
+            style={{
+              textAlign: "center",
+              fontSize: "1.5em",
+              fontWeight: "bolder",
+              color: "white"
+            }}
+          >
+            TOP MOVIES
+          </h2>
+          <br />
+          <br />
+          <Movies />
+          <br />
+          <br />
+          <br />
+
+          <br />
+          <MovieSearch />
+          <br />
+          <br />
+          <h2
+            style={{
+              textAlign: "center",
+              fontSize: "1.5em",
+              fontWeight: "bolder",
+              color: "white"
+            }}
+          >
+            CURRENT TOP FOUR SHOWS
+          </h2>
+        </div>
       </React.Fragment>
     );
   }
