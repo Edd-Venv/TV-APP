9dcde0803c1861794190350b239c9c50046d69f5
Edwin Ushibantu <51169097+Edd-Venv@users.noreply.github.com>
2019-10-29 13:33:37 +0100

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,176 +1,75 @@
 import React from "react";
-import axios from "axios";
+import { TvContext } from "../../contexts/tvContext";
+import "./Header.css";
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      data: [],
-      image1: [],
-      image2: [],
-      image3: [],
-      image4: [],
-      link1: [],
-      link2: [],
-      link3: [],
-      link4: []
-    };
-  }
-  async componentDidMount() {
-    const url = `http://api.tvmaze.com/shows/54`;
-    await axios.get(url).then(response => {
-      this.setState({
-        data: response.data,
-        image1: response.data.image,
-        isLoaded: true,
-        link1: response.data.officialSite
-      });
-    });
-    const url2 = `http://api.tvmaze.com/shows/11`;
-    await axios.get(url2).then(response => {
-      this.setState({
-        data: response.data,
-        image2: response.data.image,
-        isLoaded: true,
-        link2: response.data.officialSite
-      });
-    });
-    const url3 = `http://api.tvmaze.com/shows/52`;
-    await axios.get(url3).then(response => {
-      this.setState({
-        data: response.data,
-        image3: response.data.image,
-        isLoaded: true,
-        link3: response.data.officialSite
-      });
-    });
-    const url4 = `http://api.tvmaze.com/shows/7`;
-    await axios.get(url4).then(response => {
-      this.setState({
-        data: response.data,
-        image4: response.data.image,
-        isLoaded: true,
-        link4: response.data.officialSite
-      });
-    });
-  }
+  static contextType = TvContext;
 
   render() {
     const {
-      data,
       isLoaded,
-      image1,
-      image2,
-      image3,
-      image4,
-      link1,
-      link2,
-      link3,
-      link4
-    } = this.state;
+      headerLink1,
+      headerImage1,
+      headerLink2,
+      headerImage2,
+      headerLink3,
+      headerImage3,
+      headerLink4,
+      headerImage4
+    } = this.context;
+
     return (
-      <div>
+      <div className="flex-container">
         {isLoaded === false ? (
           <div
-            class="spinner-grow text-dark"
+            className="spinner-grow text-dark"
             role="status"
-            style={{ marginLeft: "50%" }}
+            style={{ marginLeft: "1%" }}
           >
-            <span class="sr-only">Loading...</span>
+            <span className="sr-only">Loading...</span>
           </div>
         ) : (
           <React.Fragment>
-            <span
-              style={{
-                width: "100px",
-                height: "100px",
-                position: "relative",
-                animationName: "first",
-                animationDuration: "4s",
-                animationIterationCount: "1",
-                animationTimingFunction: "ease",
-                animationFillMode: "forwards",
-                animationDelay: "-0.5s"
-              }}
-            >
-              <a href={link1} target="_blank">
+            <div className="header1">
+              <a href={headerLink1} target="_blank" rel="noopener noreferrer">
                 <img
                   alt="loading"
-                  src={image1.original}
+                  src={headerImage1.original}
                   className="img-thumbnail"
-                  style={{ width: "170px" }}
+                  id="box"
                 />
               </a>
-            </span>
-            <span
-              style={{
-                width: "100px",
-                height: "100px",
-                position: "relative",
-                animationName: "second",
-                animationDuration: "4s",
-                animationIterationCount: "1",
-                animationTimingFunction: "ease",
-                animationFillMode: "forwards",
-                animationDelay: "-0.5s"
-              }}
-            >
-              <a href={link2} target="_blank">
+            </div>
+            <div className="header2">
+              <a href={headerLink2} target="_blank" rel="noopener noreferrer">
                 <img
                   alt="loading"
-                  src={image2.original}
+                  src={headerImage2.original}
                   className="img-thumbnail"
-                  style={{ width: "170px" }}
+                  id="box"
                 />
               </a>
-            </span>
-            <span
-              style={{
-                width: "100px",
-                height: "100px",
-                position: "relative",
-                marginLeft: "30%",
-                animationName: "third",
-                animationDuration: "4s",
-                animationIterationCount: "1",
-                animationTimingFunction: "ease",
-                animationFillMode: "forwards",
-                animationDelay: "-0.5s"
-              }}
-            >
-              <a href={link3} target="_blank">
+            </div>
+            <div className="header3">
+              <a href={headerLink3} target="_blank" rel="noopener noreferrer">
                 <img
                   alt="loading"
-                  src={image3.original}
+                  src={headerImage3.original}
                   className="img-thumbnail"
-                  style={{ width: "170px" }}
+                  id="box"
                 />
               </a>
-            </span>
-            <span
-              style={{
-                width: "100px",
-                height: "100px",
-                position: "relative",
-                marginLeft: "5%",
-                animationName: "fouth",
-                animationDuration: "4s",
-                animationIterationCount: "1",
-                animationTimingFunction: "ease",
-                animationFillMode: "forwards",
-                animationDelay: "-0.5s"
-              }}
-            >
-              <a href={link4} target="_blank">
+            </div>
+            <div className="header4">
+              <a href={headerLink4} target="_blank" rel="noopener noreferrer">
                 <img
                   alt="loading"
-                  src={image4.original}
+                  src={headerImage4.original}
                   className="img-thumbnail"
-                  style={{ width: "170px" }}
+                  id="box"
                 />
               </a>
-            </span>
+            </div>
           </React.Fragment>
         )}
       </div>
