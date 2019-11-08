c63236f70525ceef84de6ef460282efad3246f8c
Edwin Ushibantu <51169097+Edd-Venv@users.noreply.github.com>
2019-11-08 10:36:52 +0100

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,125 +1,61 @@
 import React from "react";
-import Axios from "axios";
+import "./Search.css";
+import { TvSearchContext } from "../../contexts/tvSearchContext";
+import SearchResult from "./SearchResult";
 
-class Movies extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      Movies: [],
-      Movies2: [],
-      Movies3: [],
-      Movies4: []
-    };
-  }
-
-  async componentDidMount() {
-    const apiKey = "";
-    const url = `http://www.omdbapi.com/?t=Bohemian Rhapsody&apikey=${apiKey}`;
-    Axios.get(url).then(response => {
-      this.setState({
-        isLoaded: true,
-        Movies: response.data
-      });
-    });
-    const url2 = `http://www.omdbapi.com/?t=A Star Is Born&apikey=${apiKey}`;
-    Axios.get(url2).then(response => {
-      this.setState({
-        isLoaded: true,
-        Movies2: response.data
-      });
-    });
-    const url3 = `http://www.omdbapi.com/?t=Fantastic Beasts: The Crimes Of Grindelwald&apikey=${apiKey}`;
-    Axios.get(url3).then(response => {
-      this.setState({
-        isLoaded: true,
-        Movies3: response.data
-      });
-    });
-    const url4 = `http://www.omdbapi.com/?t=Jurassic World: Fallen Kingdom&apikey=${apiKey}`;
-    Axios.get(url4).then(response => {
-        this.setState({
-        isLoaded: true,
-        Movies4: response.data
-      });
-    });
-  }
-
+class Search extends React.Component {
+  static contextType = TvSearchContext;
   render() {
-    const { Movies, Movies2, Movies3, Movies4 } = this.state;
+    const {
+      data,
+      isLoaded,
+      image,
+      display,
+      summary,
+      Test,
+      ShowTrailer,
+      handleSubmit,
+      handleChange,
+      handleClick,
+      value
+    } = this.context;
     return (
       <React.Fragment>
-        <div className="movieContainer">
-          <div className="row">
-            <div
-              style={{
-                width: "240px",
-                boxSizing: "borderBox",
-                marginLeft: "3%"
-              }}
-            >
-              <img
-                alt="loading"
-                src={Movies.Poster}
-                className="img-thumbnail"
-                style={{ width: "250px", height: "auto" }}
-                id="boxMovie"
-                margin="auto"
-              />
-            </div>
-            <div
-              style={{
-                width: "240px",
-                boxSizing: "borderBox",
-                marginLeft: "10%"
-              }}
-            >
-              <img
-                alt="loading"
-                src={Movies2.Poster}
-                className="img-thumbnail"
-                style={{ width: "250px", height: "auto" }}
-                id="boxMovie"
-                margin="auto"
-              />
-            </div>
-            <div
-              style={{
-                width: "240px",
-                boxSizing: "borderBox",
-                marginLeft: "10%"
-              }}
-            >
-              <img
-                alt="loading"
-                src={Movies3.Poster}
-                className="img-thumbnail"
-                style={{ width: "250px", height: "auto" }}
-                id="boxMovie"
-                margin="auto"
-              />
-            </div>
-            <div
-              style={{
-                width: "240px",
-                boxSizing: "borderBox",
-                marginLeft: "10%"
-              }}
-            >
-              <img
-                alt="loading"
-                src={Movies4.Poster}
-                className="img-thumbnail"
-                style={{ width: "250px", height: "auto" }}
-                id="boxMovie"
-                margin="auto"
-              />
-            </div>
-          </div>
-        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <form onSubmit={handleSubmit} style={{ marginLeft: "39.3%" }}>
+          <span className="form-inline">
+            <button id="SearchButton" type="submit">
+              <i className="fas fa-search" />
+            </button>
+            <input
+              className="InputStyle"
+              type="text/number"
+              onChange={handleChange}
+              value={value}
+              placeholder="Show Title"
+              style={{ width: "35%", fontSize: "1.2em" }}
+              id="input"
+            />
+          </span>
+        </form>
+        <br />
+        <br />
+        <SearchResult
+          data={data}
+          isLoaded={isLoaded}
+          image={image}
+          display={display}
+          summary={summary}
+          Test={Test}
+          ShowTrailer={ShowTrailer}
+          handleClick={handleClick}
+        />
       </React.Fragment>
     );
   }
 }
 
-export default Movies;
+export default Search;
